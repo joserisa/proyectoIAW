201da7e2853570935bfb652bea66be8b0e630460
José María Riol Sánchez <73311874+Jose-MRS@users.noreply.github.com>
2022-12-08 13:35:31 +0100

    Base de datos Postgres añadida

--- Pyex/Pyex/settings.py
+++ Pyex/Pyex/settings.py
@@ -77,10 +77,19 @@ WSGI_APPLICATION = 'Pyex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'pyex',
+            'passfile': '.my_pgpass',
+        },
     }
 }
 
